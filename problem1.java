7dd187b2db52736b4fd98bd477f29fe716c2d56c
iamashitt <bribo60322@gmail.com>
2024-09-09 10:32:53 -0700

    a

--- problem1.java
+++ problem1.java
@@ -1,1 +1,10 @@
 // Create your class here; don't forget the main method
+public class problem1
+{
+    public static void main(String[] args)
+    {
+        int score;
+        4 = score;
+        System.out.println(score);
+    }
+}
