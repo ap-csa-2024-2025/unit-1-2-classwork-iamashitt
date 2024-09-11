a4ac46007850d50c62ad7e509c98b5bfb3075d94
iamashitt <bribo60322@gmail.com>
2024-09-11 10:44:50 -0700

    tt

--- problem1.java
+++ problem1.java
@@ -1,10 +1,8 @@
 // Create your class here; don't forget the main method
+public class problem1 {
+  public static void main(String[] args) {
+   String x = "springrolls";
+   System.out.print("I like to eat " + x + " as well!");
+  }
 
-public class problem1
-  public static void main(String[] args)
-{
-  {
-    x = "springrolls";
-    System.out.print("I like to eat " + x + "as well!");
-  } 
 }
