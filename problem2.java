e7c4b018c999ba81bc3818cc8c8491e5faeda6a4
iamashitt <bribo60322@gmail.com>
2024-09-11 11:01:52 -0700

    a

--- problem2.java
+++ problem2.java
@@ -1,1 +1,11 @@
 // Create your class here; don't forget the main method
+public class problem2 {
+  public static void main(String[] args) {
+   String x = "briair";
+   String y = "leighton";
+   String z = "Whitney";
+   String a = "Sydney"
+   System.out.print("I like to eat " + x + " as well!");
+  }
+
+}
